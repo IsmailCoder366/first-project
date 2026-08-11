b3b64381ee2256fb32992371c2268e76679322ce
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-12 00:08:29 +0500

    module file added

---
+++ module.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const os = require('os');
+
+fs.writeFileSync('data.txt', 'this is my data file')
+console.log(os.platform())
+console.log(os.hostname())
+console.log(os.cpus())
