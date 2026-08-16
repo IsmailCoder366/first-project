0aabe05c3d757698e6c9552112f571511bfbabb1
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-16 13:55:03 +0500

    form html and form js created

--- form.js
+++ form.js
@@ -1,22 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
-http.createServer((req, resp) =>{
-    resp.writeHead(200, {'content-type': 'text/html'});
-    if(req.url == '/'){
-resp.write(`
-        <form action = "/submit" method = "POST">
-        <input type = "text" placeholder = "Enter your name"/>
-        <input type = "text" placeholder = "Enter your email"/>
-        <button>Submit</button>
-        </form>
-        `);
-    }
-    else if (req.url== '/submit'){
-        resp.write(`
-            <h1> Form Submitted Successfully</h1>`)
-    }
+http.createServer((req, resp) => {
 
-            resp.end();
+    if (req.url === '/') {
 
+        fs.readFile('html/form.html', 'utf-8', (error, data) => {
+
+            if (error) {
+                resp.writeHead(500, { 'content-type': 'text/plain' });
+                resp.end('Internal server error');
+                return;
+            }
+
+            resp.writeHead(200, { 'content-type': 'text/html' });
+            resp.end(data);
+        });
+
+    } 
     
+    else if (req.url === '/submit') {
+
+        resp.writeHead(200, { 'content-type': 'text/html' });
+
+        resp.end(`
+            <h1>Form Submitted Successfully</h1>
+        `);
+
+    }
+
 }).listen(3100);
