0b47fa1a041605d600aff1e3fc50636cb77883d8
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-19 14:27:53 +0500

    sync and async pratice completed

--- syncVSasync.js
+++ syncVSasync.js
@@ -1,11 +1,19 @@
-let a = 20;
-let b = 100;
+// let a = 20;
+// let b = 100;
 
 
-setTimeout(() =>{
-    b = 0;
-    console.log(a+b)
-}, 2000)
+// setTimeout(() =>{
+//     b = 0;
+//     console.log(a+b)
+// }, 2000)
 
 
-console.log(a+b);
+// console.log(a+b);
+
+
+
+const fs = require('fs')
+
+const data = fs.readFileSync("text/peter.txt", "utf-8")
+console.log(data)
+console.log("script")
