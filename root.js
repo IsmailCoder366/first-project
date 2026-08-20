64c5899e0c654c617b9d6a4a164e1e1b0d88c3f9
IsmailCoder366 <ismailcoder366@gmail.com>
2026-08-20 08:29:16 +0500

    simple form created

--- root.js
+++ root.js
@@ -1,7 +1,13 @@
 const http = require('http')
 const userForm = require('./userForm');
 http.createServer((req, resp) => {
-    userForm(req, resp);
+    resp.writeHead(200, {"Content-type" : "text/html"})
+    if(req.url == '/'){
+        userForm(req, resp)
+    }
+    else if(req.url == '/submit'){
+        resp.write('form submitted')
+    }
     resp.end()
 }
 
--- userForm.js
+++ userForm.js
@@ -1,5 +1,11 @@
 function userForm(req, resp ){
-resp.write('hello world')
+resp.write(`
+    <form action="/submit" method="POST">
+    <input type="text" name = "name" placeholder="Enter your name">
+    <input type="text" name = "email" placeholder="Enter your email">
+    <button type="submit">Submit</button>
+    </form>
+    `)
 }
 
 module.exports = userForm
